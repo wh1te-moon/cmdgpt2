20d810745ac4e5d3280f375c06b5db92ea18acc6
wh1te-moon <wonderful0o0@stu2021.jnu.edu.cn>
2023-11-20 14:00:05 +0800

    modified:   constants.py

--- constants.py
+++ constants.py
@@ -13,7 +13,7 @@ constants = {
 input_pattern = [""]
 index = 1
 gpt3 = "gpt-3.5-turbo-16k-0613"
-gpt4 = "gpt-4-0314"
+gpt4 = "gpt-4-vision-preview"
 
 historyLocation = "./history"
 templateLocation = "./template"
